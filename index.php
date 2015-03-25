13de20d586ae8996533be19f913f5445090f14a0
jch818 <jch818@mail.usask.ca>
2015-03-24 19:00:37 -0600

    Changes to index.php

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>Afrique Hair</title>
-</head>
-
-<body>
-	The content of the document......
-</body>
-
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Afrique</title>
+	 
+	  <!-- Latest compiled and minified CSS -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	  
+	  <!-- Latest compiled and minified JavaScript -->
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+ 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+  </head>
+  <body>
+    <h1>Welcome to Afrique!</h1>
+	
+	<!-- Header -->
+	<div class="container">
+		<div>
+		</div>
+	</div>
+	
+	<!-- Main Content -->
+	<div></div>
+	
+	<!-- Footer -->
+	<div></div>
+ 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
