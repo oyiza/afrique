92ee72140eaacfcbf46279c15dc28ae48079ce7c
Praise <pie654@mail.usask.ca>
2015-03-25 15:56:08 -0600

    Progress
    Added copyright footer and carousel speed changer

--- index.php
+++ index.php
@@ -114,7 +114,15 @@
 	</div>
 	
 	<!-- Footer -->
-	<div></div>
+	  <footer>
+        <div class="container">
+			  <div class="row">
+				  <div class="col-lg-12 text-center">
+					  <p>Copyright &copy; Afrique 2015</p>
+				  </div>
+			  </div>
+        </div>
+	  </footer>
 	
 	
  
@@ -122,6 +130,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+	
+	  <!-- Script to Activate the Carousel (Change as needed: smaller interval = faster speed)-->
+	  <script>
+		  $('.carousel').carousel({
+			  interval: 5000 //changes the speed
+		  })
+	  </script>
 	
   </body>
 </html>
