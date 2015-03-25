69f2d758ca1bea4c71e54306c576d05e055ae516
jch818 <jch818@mail.usask.ca>
2015-03-24 19:17:41 -0600

    Changes
    Adding style.css.
    changes to index.php

--- index.php
+++ index.php
@@ -43,8 +43,55 @@
 		</div>
 	</div> <!-- end of header -->
 	
+	<!-- Carousel -->
+	
+	 <div class="container">
+		  <br>
+		  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+			  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			  <li data-target="#myCarousel" data-slide-to="1"></li>
+			  <li data-target="#myCarousel" data-slide-to="2"></li>
+			  <li data-target="#myCarousel" data-slide-to="3"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+			  <div class="item active">
+				<img src="img_chania.jpg" alt="Chania" width="460" height="345">
+			  </div>
+
+			  <div class="item">
+				<img src="img_chania2.jpg" alt="Chania" width="460" height="345">
+			  </div>
+			
+			  <div class="item">
+				<img src="img_flower.jpg" alt="Flower" width="460" height="345">
+			  </div>
+
+			  <div class="item">
+				<img src="img_flower2.jpg" alt="Flower" width="460" height="345">
+			  </div>
+			</div>
+
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			  <span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			  <span class="sr-only">Next</span>
+			</a>
+		  </div>
+		</div>
+
+	
+	<!-- CAROUSEL ENDING -->
+	
 	<!-- Main Content -->
-	<div>
+	<div class="container">
 		 <h1>Welcome to Afrique!</h1>
 	</div>
 	
