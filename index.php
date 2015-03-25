54ff851525f8f25d77e6ad11017dc829b58ef0ab
Praise <pie654@mail.usask.ca>
2015-03-25 15:43:26 -0600

    added navbar-brand to menu buttons
    Buttons now collapse when the screen is minimized

--- index.php
+++ index.php
@@ -36,7 +36,18 @@
 		<div>
 			<nav class="navbar navbar-default">
 			  <div class="container-fluid">
-				<div>
+				  <!-- Brand and toggle get grouped for better mobile display -->
+				  <div class="navbar-header">
+					  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+					  </button>
+					  <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+					  <a class="navbar-brand" href="index.php">Afrique</a>
+				  </div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				  <ul class="nav navbar-nav">
 					<li class="active"><a href="#">Home</a></li>
 					<li><a href="#">About Us</a></li>
@@ -44,6 +55,7 @@
 					<li><a href="#">Contact Us</a></li> 
 				  </ul>
 				</div>
+				<!-- /.navbar-collapse -->
 			  </div>
 			</nav>	
 		</div>
