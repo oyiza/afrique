769950963fbbc5c80fdc63aeb697d77c9c644297
jch818 <jch818@mail.usask.ca>
2015-03-25 22:52:34 -0600

    addition of images and changes to menu

---
+++ aboutus.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Afrique</title>
+	
+	<!-- Style.css -->
+	 <link rel="stylesheet" type="text/css" href="css/style.css">
+	 
+	  <!-- Latest compiled and minified CSS -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+ 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+  </head>
+  <body>
+	<!-- Header -->
+	<div class="container">
+		<div id="header">
+			<img src="http://swift.bridgeitsolutions.ca/wp-content/uploads/2015/03/afriquelogo.png" alt="logo">
+		</div>
+		
+		<!-- MENU BAR -->
+		<div>
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				  <!-- Brand and toggle get grouped for better mobile display -->
+				  <div class="navbar-header">
+					  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+					  </button>
+					  <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+					  <a class="navbar-brand" href="index.php">Afrique</a>
+				  </div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
+					<li><a href="#">Book Appointment</a></li> 
+					<li><a href="#">Contact Us</a></li> 
+					<li><a href="signIn.php"><strong>Sign In</strong></a></li>
+				  </ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			  </div>
+			</nav>	
+		</div>
+		
+	</div> <!-- end of header -->
+	
+	<!-- main content -->
+	
+		<p> about us at here</p>
+		
+	<!-- Footer -->
+	  <footer>
+        <div class="container">
+			  <div class="row">
+				  <div class="col-lg-12 text-center">
+					  <p>Copyright &copy; Afrique 2015</p>
+				  </div>
+			  </div>
+        </div>
+	  </footer>
+	
+  </body>
+  </html>
