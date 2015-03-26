abf930f678277328aeebf4ab7abe1616c58923e6
jch818 <jch818@mail.usask.ca>
2015-03-25 22:37:53 -0600

    Addition of images

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				  <ul class="nav navbar-nav">
 					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#">About Us</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
 					<li><a href="#">Book Appointment</a></li> 
 					<li><a href="#">Contact Us</a></li> 
 					<li><a href="signIn.php"><strong>Sign In</strong></a></li>
