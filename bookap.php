905974cb2d39a1b45c96d37e83b502140930cb56
Praise <pie654@mail.usask.ca>
2015-03-26 22:27:59 -0600

    Added submit button to bookap.php

--- bookap.php
+++ bookap.php
@@ -68,7 +68,7 @@
 		<div class="row">
 			 <div class="box">
 				<div class="col-lg-12 text-center">
-				<div class="panel panel-body">
+				<div class="text-center" style="background-color: #778899">
 					<hr>
 					<h2 class="intro-text text-center"><strong>Booking Form</strong></h2>
 					<hr>
@@ -109,6 +109,12 @@
 								<label>Are you buying hair or do you have your own?</label>
 								<input type="text" class="form-control" placeholder="">
 							</div>
+						</div>
+						<div class="form-group col-lg-12">
+							<button type="submit" class="btn btn-default">Submit</button>
+						</div>
+						<div class="row">
+							
 						</div>
 					</form>
 				</div>
