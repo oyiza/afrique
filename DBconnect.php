9f556beab171262e1b602b299e78b7902da0da61
Praise <pie654@mail.usask.ca>
2015-03-27 22:34:59 -0600

    emptied DBConnect

--- DBconnect.php
+++ DBconnect.php
@@ -15,26 +15,14 @@
 		    if($conn->connect_error)
 				die("Connection failed: ".$conn->connect_error);
 		    else
-				echo "Connection successfully</br>";
+				echo "Connected successfully</br>";
 			
-			$sql = "CREATE TABLE AddressBook(
-				id INT AUTO_INCREMENT PRIMARY KEY,
-				firstname VARCHAR(30) NOT NULL,
-				lastname VARCHAR(30) NOT NULL,
-				company VARCHAR(30),
-				phone INT NOT NULL,
-				email VARCHAR(255) NOT NULL,
-				url VARCHAR(255),
-				address VARCHAR(255) NOT NULL,
-				birthday DATE,
-				add_date DATE NOT NULL,
-				note VARCHAR(255)
-			)";
+			$sql = "";
 			
-			if($conn->query($sql) == TRUE)
-				echo "Table AddressBook created succefully";
-			else
-				echo "Error creating table: ".$conn->error;
+			//if($conn->query($sql) == TRUE)
+				//echo "Table AddressBook created succefully";
+			//else
+				//echo "Error creating table: ".$conn->error;
 			
 		 ?> 
 	</body>
