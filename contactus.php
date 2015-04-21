03093e43608eeabaea9234763235feb8eed82b1c
Praise <pie654@mail.usask.ca>
2015-04-21 12:19:06 -0600

    tiny tweaks

--- contactus.php
+++ contactus.php
@@ -74,7 +74,7 @@
 
 		<div class="text-center form center-div" id="booking">
 			<br/>
-			<h2 class="intro-text text-center form-head"><strong>Booking Form</strong></h2>
+			<h2 class="intro-text text-center form-head"><strong>Contact Form</strong></h2>
 		<form class="form-horizontal" role="form" method="post" action="contactus.php">
 			<div class="form-group">
 				<label for="name" class="col-sm-2 control-label">Name</label>
@@ -132,7 +132,7 @@
 	   mail($to, $subject, $message, "from: " . $name);
 	   echo "Your message Has been sent";
 	   
-	}
+		}
 	?>
 		
 	
